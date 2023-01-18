321e655532263be356e5ffd5d97c8ed004f1bce4
Zilia Bairamova <ziliabairamova@gmail.com>
2023-01-18 20:05:12 +0300

    build.gradle and test chenged

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -18,15 +18,18 @@ public class CardDeliveryTest {
     @Test
     public void shouldChooseADate (){
 
-        open("http://localhost:9999/");
-        $("[placeholder='Город'] input").setValue("Казань");
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Казань");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(generateDate());
-        $("[name='name'] input").setValue("Иванов Иван");
-        $("[name='phone'] input").setValue("+79879879879");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79879879879");
         $("[data-test-id='agreement']").click();
         $$("button").findBy(Condition.text("Забронировать")).click();
-        $(byText("Встреча успешно забронирована на ")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate()), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
